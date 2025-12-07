998d88debac88ff86893e719f0811cc6e6eb7906
Max <Max@gmail.com>
2025-12-07 13:53:09 +0300

    change docker compose volumes

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-RUN mkdir -p "static/images"
+RUN mkdir -p static/images && mkdir -p "data" #створюєм папки для статичних файлів та фото а також папку для
 
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_async_engine('sqlite+aiosqlite:///./menu.db', connect_args={'check_same_thread': False})
+engine = create_async_engine('sqlite+aiosqlite:///./data/menu.db', connect_args={'check_same_thread': False})
 
 Sessionlocal = async_sessionmaker(bind=engine, autocommit=False, autoflush=False,class_=AsyncSession )
 
